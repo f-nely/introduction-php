888ddabcb4137386cb0b03f1b5b3345707ba0d85
nely de faria <nelly.nf54@gmail.com>
2022-05-31 17:25:39 -0300

    Criando arquivo operador-ternario.php

---
+++ introduction-php/operador-ternario.php
@@ -0,0 +1,11 @@
+<?php
+
+$age = 17;
+
+echo $age >= 18 ? 'de maior' : 'de menor';
+echo '<br>';
+
+// $nome = 'Ale';
+// echo isset($nome) ? $nome : 'nome não definido';
+
+echo $nome ?? 'nome não definido';
