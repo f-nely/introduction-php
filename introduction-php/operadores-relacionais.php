d9d6fe36d92889acf2f2301c56c1572522e4f3f6
nely de faria <nelly.nf54@gmail.com>
2022-05-31 17:25:06 -0300

    Criando arquivo operadores-relacionais.php

---
+++ introduction-php/operadores-relacionais.php
@@ -0,0 +1,18 @@
+<?php
+
+$numero = 10;
+
+/*
+    == Igualdade
+    === Identicidade
+
+    > maior que
+    < menor que
+    >= maior igual a que
+    <= menor igual a que
+
+    ! negação
+    != negar a igualdade
+    !== negar a identicidade
+*/
+var_dump($numero === 10);
