6a2a0d2a6024e403df62bc25a86fa5a1d0cbff45
nely de faria <nelly.nf54@gmail.com>
2022-05-31 17:21:13 -0300

    Criando arquivo tipos_de_dados.php

---
+++ introduction-php/tipos_de_dados.php
@@ -0,0 +1,8 @@
+<?php
+
+$tenhoEmprego = true; // booleano
+$numeroCasa = 2006; // integer
+$cpf = '012.123.456-10'; // string
+$saldoBancario = -2569.89; // float
+
+var_dump($tenhoEmprego, $numeroCasa, $cpf, $saldoBancario);
