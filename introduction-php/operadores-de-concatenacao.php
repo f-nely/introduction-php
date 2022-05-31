023c26e93d264d1e9c3a227010bafec03fba7c0a
nely de faria <nelly.nf54@gmail.com>
2022-05-31 17:24:26 -0300

    Criando arquivo operadores-de-concatenacao.php

---
+++ introduction-php/operadores-de-concatenacao.php
@@ -0,0 +1,16 @@
+<?php
+
+$firstName = 'Alessandro';
+$lastName = 'Feitoza';
+$idade = 30;
+$cidade = 'Fortaleza';
+
+echo $firstName . ' ' . $lastName;
+echo '<br>';
+
+$nome = 'Diana';
+$nome .= 'Arnos';
+echo $nome;
+echo '<br>';
+
+echo "Eu me chamo {$firstName} {$lastName} tenho {$idade} e moro em {$cidade}.";
