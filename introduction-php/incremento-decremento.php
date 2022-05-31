c7b43eb6c71efa82dfcb953f6e986e415365ee0f
nely de faria <nelly.nf54@gmail.com>
2022-05-31 17:23:44 -0300

    Criando arquivo incremento-decremento.php

---
+++ introduction-php/incremento-decremento.php
@@ -0,0 +1,19 @@
+<?php
+
+$salario = 2000;
+$salarioDoAlessandro = 3000;
+
+$salario = $salario + 200; // 2200
+$salarioDoAlessandro = $salarioDoAlessandro + 200; // 3200
+
+$salario += 200; // 2400
+$salarioDoAlessandro += 200; // 3400
+
+$salario -= 500; // 1900 
+echo $salario; 
+
+$numero = 20;
+
+$numero = $numero + 1; // 21
+$numero +=1; // 22
+$numero++; // 23
