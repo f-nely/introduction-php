ee2c914a8819baf366a9f2a94d537f3f9d329090
nely de faria <nelly.nf54@gmail.com>
2022-06-01 17:10:56 -0300

    Criando arquivo estrutura-de-repeticao.php

---
+++ introduction-php/estrutura-de-repeticao.php
@@ -0,0 +1,24 @@
+<?php
+
+// while
+$j = 1;
+
+while ($j <= 10) {
+    echo "Carro pancadão hey {$j} <br>";
+    $j++;
+}
+
+echo '<br>';
+
+// for
+for ($i = 0; $i <= 10; $i++) {
+    echo "$i <br>";
+}
+
+// do while
+$n = 1;
+
+do {
+    echo "Olha o bonde passando<br>";
+    $n++;
+} while ($n <= 10);
