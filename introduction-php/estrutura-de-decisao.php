582dd51a9f9bdfee38704f81d845a97b01a167e9
nely de faria <nelly.nf54@gmail.com>
2022-06-01 17:09:13 -0300

    Criando arquivo estrutura-de-decisao.php

---
+++ introduction-php/estrutura-de-decisao.php
@@ -0,0 +1,23 @@
+<?php
+
+$n = -110;
+
+/* if ($n > 0) {
+    echo 'Positivo';
+} else {
+    if ($n < 0) {
+        echo 'Negativo';
+    } else {
+        echo 'Nulo';
+    } 
+} */
+
+if ($n > 0) {
+    echo 'Positivo';
+} elseif ($n < 0) {
+    echo 'Negativo';
+} else {
+    echo 'Nulo';
+}
+
+echo "<br> Pronto, terminou.";
