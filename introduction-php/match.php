29843cb0e61d8afad9bb3e76d451520d146f64e7
nely de faria <nelly.nf54@gmail.com>
2022-06-01 17:10:20 -0300

    Criando arquivo match.php

---
+++ introduction-php/match.php
@@ -0,0 +1,19 @@
+<?php
+
+$mes = 5;
+
+echo match ($mes) {
+    1 => 'Janeiro',
+    2 => 'Fevereiro',
+    3 => 'Março',
+    4 => 'Abril',
+    5 => 'Maio',
+    6 => 'Juno',
+    7 => 'Julho',
+    8 => 'Julho',
+    9 => 'Setembro',
+    10 => 'Outubro',
+    11 => 'Novembro',
+    12 => 'Dezembro',
+    default => 'Mês inválido'
+};
