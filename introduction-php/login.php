b52614306b09f8a5f30220ad001d41baf4e151e7
nely de faria <nelly.nf54@gmail.com>
2022-06-03 18:05:18 -0300

    Criando arquivo login.php

---
+++ introduction-php/login.php
@@ -0,0 +1,9 @@
+<h1>Login</h1>
+<hr>
+<form action="">
+    <input type="email" name="email" id="email" placeholder="Seu email" class="form-control mb-3">
+
+    <input type="password" name="password" id="password" placeholder="Senha" class="form-control mb-3">
+
+    <button class="btn btn-primary">Enviar</button>
+</form>
