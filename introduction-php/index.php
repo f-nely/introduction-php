8c31713c15d1aa6be9dbd95cffa476aa7087ee47
nely de faria <nelly.nf54@gmail.com>
2022-06-03 15:52:10 -0300

    Criando rotas

--- introduction-php/index.php
+++ introduction-php/index.php
@@ -1,3 +1,20 @@
 <?php
 
-echo date('d/m/Y H:i');
+// echo date('d/m/Y H:i');
+include 'header.php';
+
+$url = $_SERVER['REQUEST_URI'];
+
+if ($url === '/introduction-php/') {
+    // echo '<h1>Página Inicial</h1>';
+    include 'home.php';
+} elseif ($url == '/introduction-php/login') {
+    // echo '<h1>Página de Login</h1>';
+    include 'login.php';
+} elseif ($url === '/introduction-php/cadastro') {
+    echo '<h1>Página de Cadastro</h1>';
+} else {
+    echo '<h1>Página não encontrada</h1>';
+}
+
+include 'footer.php';
