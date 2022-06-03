a87b068249a6bf1984cc48f33ae291ff6fced949
nely de faria <nelly.nf54@gmail.com>
2022-06-03 18:04:42 -0300

    Criando arquivo cadastro.php

---
+++ introduction-php/cadastro.php
@@ -0,0 +1,11 @@
+<h1>Cadastro</h1>
+<hr>
+<form action="">
+    <input type="text" name="nome" id="nome" placeholder="Seu nome">
+
+    <input type="email" name="email" id="email" placeholder="Seu email">
+
+    <input type="password" name="password" id="password" placeholder="Senha">
+
+    <button>Enviar</button>
+</form>
