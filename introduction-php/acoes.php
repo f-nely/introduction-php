cf415f8a80661e8c0922b8aef8ea91d8e1bb1540
nely de faria <nelly.nf54@gmail.com>
2022-06-03 18:33:41 -0300

    Criando arquivo acoes.php

---
+++ introduction-php/acoes.php
@@ -0,0 +1,26 @@
+<?php
+
+function login(): void
+{
+    include 'login.php';
+}
+
+function cadastro(): void
+{
+    include 'cadastro.php';
+}
+
+function home(): void
+{
+    include 'home.php';
+}
+
+function listar(): void
+{
+    include 'listar.php';
+}
+
+function pageNotFound(): void
+{
+    include '404.php';
+}
