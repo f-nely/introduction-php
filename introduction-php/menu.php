7d106c273f740a2e8c2657094756248d50ba1abf
nely de faria <nelly.nf54@gmail.com>
2022-06-03 18:05:47 -0300

    Criando arquivo menu.php

---
+++ introduction-php/menu.php
@@ -0,0 +1,5 @@
+<a href="/" class="btn btn-outline-dark">Início</a>
+<a href="/login" class="btn btn-outline-dark">Login</a>
+<a href="/cadastro" class="btn btn-outline-dark">Cadastro</a>
+<a href="/admin" class="btn btn-outline-dark">Admin</a>
+<hr>
