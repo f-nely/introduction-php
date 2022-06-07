f464941ea7ab72e764a6321e4aab8baf74062937
nely de faria <nelly.nf54@gmail.com>
2022-06-07 14:11:51 -0300

    Criando arquivo foreach.php

---
+++ introduction-php/foreach.php
@@ -0,0 +1,12 @@
+<?php
+
+$fruits = [
+    'Banana',
+    'Tangerina',
+    'Abacate',
+    'Jaca'
+];
+
+foreach ($fruits as $position => $eachFruit) {
+    echo "Position: {$position} | Fruit: {$eachFruit}" . PHP_EOL;
+}
